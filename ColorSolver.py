0e1c61adb37d052166b9c4026418dfbd71a84c6e
Tom Ellis <tellis@Mervyn-Pumpkinhead.local>
2014-03-25 22:00:11 -0400

    New brute force with eval

--- ColorSolver.py
+++ ColorSolver.py
@@ -5,81 +5,66 @@ import copy
 import operator
 
 class BoardSolver(object):
-  def __init__(self, board=None, board_x=None,n_colors=None ):
-    if board is None:
-      self.board = cb.Board(board_x=board_x,n_colors=n_colors)
-    else:
-      self.board = cb.Board(board=board)
+  def __init__(self, graph_dict, node_info, attached=[0]):
+    self.graph = graph_dict
+    self.node_info = node_info
+    self.attached = attached
 
-  def solve(self, max_depth, eval_depth, paths, type='brute'):
+  def solve(self, max_depth, max_threads, moves_between_eval=1):
     self.max_depth = max_depth
-    self.eval_depth = eval_depth
+    self.max_threads = max_threads
     self.completed = False
+    self.moves_between_eval = moves_between_eval
     start_time = time.time()
-    self.brute(copy.deepcopy(self.board), 0, paths, self.board.matched)
+    self.brute(self.graph, self.attached, depth=0, eval_depth=1)
     print 'Done in:', time.time() - start_time
     if self.completed:
       print self.max_depth, 'moves to completion'
     else:
       print 'Not completed'
 
-  def brute(self, board, depth, paths, prior_matches):
-    #If our new configuration isn't better than the last (more matched squares) return
-    if depth > 0:
-      if board.matched == prior_matches:
-        #print 'not improved'
-        return
+  def brute(self, graph, attached, depth, eval_depth):
+    if depth == self.max_depth:
+      return -1
 
-    #If we have more colors left than turns before we hit the max, return
-    if depth > self.max_depth - len(board.remaining_colors):
-      #print 'not enough moves left'
-      return
-
-    # If the board is complete, and the number of moves is better than the last try,
-    # reset max_depth. Return.
-    if board.is_complete:
-      #print 'completed', depth
-      self.completed = True
+    if graph.empty:
       if depth < self.max_depth:
         self.max_depth = depth
+        self.completed = True
+        return -2
+
+    if eval_depth == 0:
+      return self.eval(graph, attached)
+
+    adjacent_nodes = graph.adjacent[graph.node.isin(attached)]
+    available_colors = self.node_info.color[self.node_info.node.isin(adjacent_nodes)]
+    print available_colors.tolist()
+    print adjacent_nodes.tolist()
+
+    scores = {}
+    for color in available_colors:
+      correct_color_nodes = self.node_info.node[self.node_info.color == color]
+      matched_nodes = correct_color_nodes[correct_color_nodes.isin(adjacent_nodes)]
+      new_graph = graph[graph.adjacent.isin(matched_nodes) == False]
+      new_attached = attached + matched_nodes.tolist()
+      scores[color] = self.brute(new_graph, new_attached, depth + 1, eval_depth - 1)
+
+    print scores
+
+    if -2 in scores.values():
       return
 
-    colors = set(board.remaining_colors) - set([board.current_color])
-    matched = board.matched
+    color = max(scores.iteritems(), key=operator.itemgetter(1))[0]
+    correct_color_nodes = self.node_info.node[self.node_info.color == color]
+    matched_nodes = correct_color_nodes[correct_color_nodes.isin(adjacent_nodes)]
+    new_graph = graph[graph.adjacent.isin(matched_nodes) == False]
+    new_attached = attached + matched_nodes.tolist()
+    self.brute(new_graph, new_attached, depth + 1, self.moves_between_eval)
 
-    for c in colors:
-      b = cb.Board(board=board.board)
-      b.take_turn(c)
-      self.brute(b, depth + 1, paths, matched)
 
-  def eval(self, board, depth, sequence, prior_matches):
-    if board.matched == prior_matches:
-      #print 'not improved'
-      return [(sequence, 0)]
-
-    # If the board is complete, and the number of moves is better than the last try,
-    # reset max_depth. Return.
-    if board.is_complete:
-      #print 'completed', depth
-      self.completed = True
-      if depth < self.max_depth:
-        self.max_depth = depth
-      return [(sequence, 1000)]
-
-    if depth == self.eval_depth:
-      return [(sequence, board.matched - prior_matches)]
-
-    moves = []
-
-    colors = board.remaining_colors
-    for c in colors:
-      c_seq = [x for x in sequence]
-      c_seq.append(c)
-      b = cb.Board(board=board.board)
-      b.take_turn(c)
-      moves = moves + self.eval(b, depth + 1, c_seq, prior_matches)
-
-    return moves
+  def eval(self, graph, attached):
+    adjacent_nodes = graph.adjacent[graph.node.isin(attached)]
+    return adjacent_nodes.count()
 
 
   def dumb(self, board, depth):
